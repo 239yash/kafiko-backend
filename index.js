3511872c0a08df489d8a598772b5bf916bbf6da0
239yash <239yash@gmail.com>
2023-06-03 09:45:34 +0530

    add: initial commit for connection testing

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { Kafka } = require("kafkajs");
+const app = express();
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const getTopics = async () => {
+  const admin = kafka.admin();
+  await admin.connect();
+  const topics = await admin.listTopics();
+  await admin.disconnect();
+  return topics;
+};
+
+// const topicName = 'orderCreated2';
+
+// const process  = async () => {
+//     const admin = kafka.admin();
+//     await admin.connect();
+//     await admin.createTopics({
+//         topics: [{
+//             topic: topicName,
+//             numPartitions: 2,
+//             replicationFactor: 1
+//         }
+//         ],
+// });
+//     await admin.disconnect();
+// };
+
+// process().then(() => console.log('done'));
+
+getTopics()
+  .then((topics) => {
+    console.log("Kafka topics:", topics);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+app.use(express.json());
+// app.use(cors());
+// app.options("*", cors());
